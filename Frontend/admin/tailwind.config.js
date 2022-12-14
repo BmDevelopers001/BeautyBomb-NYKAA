becbc72a6918421cc91d734961ac06834d388798
iamgauhar <iamgauhar@yahoo.com>
2022-12-14 21:39:16 +0530

    Working on Admin panne;

---
+++ Frontend/admin/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tw-elements/dist/plugin')
+    // require('tail')
+  ],
+}
